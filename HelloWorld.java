d289e542b505f44c4bbaa66fead83b094e74cd32
HabeebAhmed <1habeeb.ahmed1@gmail.com>
2021-04-17 15:49:06 -0400

    added paddle

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,23 +1,75 @@
+import java.io.IOException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.*;
+import javax.swing.*;
+
 public class HelloWorld {
 
-	static final int WIDTH = 30;
-	static final int HEIGHT = 50;
+	static final int WIDTH = 20;
+	static final int HEIGHT = 30;
 
 	// The position of the ball
 	static int ballX = 3;
 	static int ballY = 6;
 
+	// The position of the paddle
+	static int paddleCenterX = 3;
+	static int paddleY = HEIGHT - 2;
+
 	// The velocity of the ball
 	static int ballVx = 1;
 	static int ballVy = 1;
+
+	static int lastInput = -1;
+
+	public static void listenForKeyboardInput() { 
+    	final JFrame frame = new JFrame();  
+        frame.setUndecorated(true);  
+        frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);  
+        
+        frame.addKeyListener(new KeyListener() {
+            @Override 
+            public void keyPressed(KeyEvent e) {  
+            	lastInput = e.getKeyCode();
+            } 
+
+            @Override 
+            public void keyReleased(KeyEvent e) {} 
+
+            @Override 
+            public void keyTyped(KeyEvent e) {}  
+        });
+
+        frame.setVisible(true);
+    }
 
 	private static void clearScreen() {  
 	    System.out.print("\033[H\033[2J");  
 	    System.out.flush();  
 	}  
-
+		
 	// returns false if game is over else return true
 	private static boolean updateState() {
+		// Move paddle
+		// 37 left
+		// 39 right
+		if (lastInput == 37) {
+			paddleCenterX -= 1;
+			lastInput = -1;
+		} else if (lastInput == 39) {
+			paddleCenterX += 1;
+			lastInput = -1;
+		}
+		if (paddleCenterX <= 1) {
+			paddleCenterX = 2;
+		}
+
+		if (paddleCenterX >= WIDTH - 2) {
+			paddleCenterX = WIDTH - 3;
+		}
+
+		// Move the ball
 		ballX += ballVx;
 		ballY += ballVy;
 
@@ -41,7 +93,11 @@ public class HelloWorld {
 		for (int y = 0 ;  y < HEIGHT ; y++) {
 			StringBuilder stringBuilder = new StringBuilder("");
 			for (int x = 0 ; x < WIDTH ; x++) {
-				if (y == 0 || y == (HEIGHT - 1)) {
+				if (y == paddleY
+					&& x >= (paddleCenterX - 1)
+					&& x <= (paddleCenterX + 1)) {
+					stringBuilder.append("=");	
+				} else if (y == 0 || y == (HEIGHT - 1)) {
 					stringBuilder.append("x");
 				} else if (x == 0 || x == (WIDTH -1)) {
 					stringBuilder.append("x");
@@ -57,17 +113,19 @@ public class HelloWorld {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
+		listenForKeyboardInput();
 		while (true) {
 			boolean canGameContinue = updateState();
 			Thread.sleep(100);
 			clearScreen();
-			if (canGameContinue) {
+			//if (canGameContinue) {
 				renderFrame();
-			} else {
-				break;
-			}
+				System.out.println(lastInput);
+			//} else {
+			//	break;
+			//}
 		}
 
-		System.out.println("Game Over");
+		//System.out.println("Game Over");
 	}
 }
