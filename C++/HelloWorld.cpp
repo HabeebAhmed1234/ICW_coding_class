40af0d6860bf041eae345e8e3d13c552f33f6d05
Habeeb Ahmed <habeebahmed@google.com>
2021-08-13 22:16:04 -0400

    made new folder for java and c++

---
+++ C++/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int y = 0 ; y < 8 ; y++) {
+        for (int x = 0 ; x < 8 ; x++) {
+            cout<<" "<<"X";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
