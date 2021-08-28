ba76edc1e221a49d7f949afdcc4b07c14be00c74
Habeeb Ahmed <habeebahmed@google.com>
2021-08-27 22:14:51 -0400

    lesson 4 code

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -2,12 +2,32 @@
 
 using namespace std;
 
-int main() {
-    for (int y = 0 ; y < 8 ; y++) {
-        for (int x = 0 ; x < 8 ; x++) {
-            cout<<" "<<"X";
+const char PAWN = 'P';
+const char EMPTY = '_';
+const string SPECIAL_PIECES = "RKBQ$BKR";
+
+char** board = new char*[8];
+
+void intializeBoard() {
+    for (int row = 0 ; row < 8 ; row++) {
+        board[row] = new char[8];
+        for (int col = 0 ; col < 8 ; col++) {
+            board[row][col] = 'X';
+        }
+    }
+}
+
+void printBoard() {
+    for (int row = 0 ; row < 8 ; row++) {
+        for (int col = 0 ; col < 8 ; col++) {
+            cout<<' '<<board[row][col];
         }
         cout<<endl;
     }
+}
+
+int main() {
+    intializeBoard();
+    printBoard();
     return 0;
 }
