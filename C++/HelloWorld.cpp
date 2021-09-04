c956f695750989961fd9742a8fec30a6191b23f1
Habeeb Ahmed <habeebahmed@google.com>
2021-09-03 22:02:54 -0400

    Lesson 5

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const char PAWN = 'P';
 const char EMPTY = '_';
-const string SPECIAL_PIECES = "RKBQ$BKR";
+const string SPECIAL_PIECES = "RKB$QBKR";
 
 char** board = new char*[8];
 
@@ -12,7 +12,20 @@ void intializeBoard() {
     for (int row = 0 ; row < 8 ; row++) {
         board[row] = new char[8];
         for (int col = 0 ; col < 8 ; col++) {
-            board[row][col] = 'X';
+            char cellValue = 'X'; 
+            switch(row) {
+                case 0:
+                    cellValue = SPECIAL_PIECES[col];
+                    break;
+                case 7:
+                    cellValue = SPECIAL_PIECES[col];
+                    break;
+                case 1:
+                case 6:
+                    cellValue = PAWN;
+                    break;
+            }
+            board[row][col] = cellValue;
         }
     }
 }
